8e9f5fcb6848c2acf906d3c8a9ff7ef708cddb16
Girish V <vgirish10@gmail.com>
2020-10-05 19:43:07 +0530

    Intial Sails integration with ReactJS

---
+++ assets/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log('hey there!!');
+
+const App = () => {
+    return (<div><h1> Flight Search Application</h1></div>);
+};
+ReactDOM.render(<App />, document.getElementById('root'));
